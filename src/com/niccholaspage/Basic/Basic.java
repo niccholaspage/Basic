f1744d112433827ccc22deb1c754eb569d18dc98
Nicholas "niccholaspage" Nassar <niccholaspage@gmail.com>
2011-05-29 16:08:04 -0400

    Cleanup

--- src/com/niccholaspage/Basic/Basic.java
+++ src/com/niccholaspage/Basic/Basic.java
@@ -6,11 +6,9 @@ import org.bukkit.event.Event;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Basic extends JavaPlugin {
-	//Links the BasicPlayerListener
 	private final BasicPlayerListener playerListener = new BasicPlayerListener(this);
     @Override
 	public void onDisable() {
-		//Print "Basic Disabled" on the log.
 		System.out.println("Basic Disabled");
 	}
     @Override
@@ -18,11 +16,10 @@ public class Basic extends JavaPlugin {
 		//Create the pluginmanage pm.
 		PluginManager pm = getServer().getPluginManager();
 		//Create PlayerCommand listener
-	    pm.registerEvent(Event.Type.PLAYER_COMMAND_PREPROCESS, this.playerListener, Event.Priority.Normal, this);
+	    pm.registerEvent(Event.Type.PLAYER_COMMAND_PREPROCESS, playerListener, Event.Priority.Normal, this);
        //Get the infomation from the yml file.
         PluginDescriptionFile pdfFile = this.getDescription();
         //Print that the plugin has been enabled!
         System.out.println(pdfFile.getName() + " version " + pdfFile.getVersion() + " is enabled!");
-		
 	}
 }
